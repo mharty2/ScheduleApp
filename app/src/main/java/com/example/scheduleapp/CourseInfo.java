224de45ebb4d699e0b1bdc4472d0d8fa2da927ea
Michael Harty <mharty2@illinois.edu>
2019-04-23 18:16:14 -0500

    all UI roughly done

---
+++ app/src/main/java/com/example/scheduleapp/CourseInfo.java
@@ -0,0 +1,11 @@
+package com.example.scheduleapp;
+/** Class for holding all data for a single course */
+public class CourseInfo {
+    String name;
+    int crn;
+    String location;
+    String time;
+    int creditHours;
+    String days;
+    String type;
+}
