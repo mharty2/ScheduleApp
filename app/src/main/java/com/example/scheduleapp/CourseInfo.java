ce14be5fd5781c586ad796b9c0e985fea3a8fa5f
Michael Harty <mharty2@illinois.edu>
2019-04-26 15:08:34 -0500

    added base for firebase

--- app/src/main/java/com/example/scheduleapp/CourseInfo.java
+++ app/src/main/java/com/example/scheduleapp/CourseInfo.java
@@ -1,5 +1,5 @@
 package com.example.scheduleapp;
-/** Class for holding all data for a single course */
+/** Class for holding all data for google-services.json single course */
 public class CourseInfo {
     String name;
     int crn;
--- app/src/test/java/com/example/scheduleapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/scheduleapp/ExampleUnitTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <google-services.json href="http://d.android.com/tools/testing">Testing documentation</google-services.json>
  */
 public class ExampleUnitTest {
     @Test
