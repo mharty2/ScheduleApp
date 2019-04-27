4284146668a78b3c239d12447562fe4f8d820780
Michael Harty <mharty2@illinois.edu>
2019-04-27 15:00:59 -0500

    switched from xml to Activities.

--- app/src/main/java/com/example/scheduleapp/ChooseSchedule.java
+++ app/src/main/java/com/example/scheduleapp/ChooseSchedule.java
@@ -3,7 +3,7 @@ package com.example.scheduleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChooseSchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/scheduleapp/loadSchedule.java
+++ app/src/main/java/com/example/scheduleapp/loadSchedule.java
@@ -3,11 +3,11 @@ package com.example.scheduleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class loadSchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_schedule);
+        setContentView(R.layout.activity_load_schedule);
     }
 }
