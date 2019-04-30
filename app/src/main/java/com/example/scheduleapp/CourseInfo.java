878bc9bf67ac974f2ba1e22f5af993a779742c12
Michael Harty <mharty2@illinois.edu>
2019-04-30 13:12:34 -0500

    courseInfo CardView done

--- app/src/main/java/com/example/scheduleapp/CourseInfo.java
+++ app/src/main/java/com/example/scheduleapp/CourseInfo.java
@@ -1,11 +1,55 @@
 package com.example.scheduleapp;
 /** Class for holding all data for google-services.json single course */
 public class CourseInfo {
-    String name;
-    int crn;
-    String location;
-    String time;
-    int creditHours;
-    String days;
-    String type;
+    private String name;
+    private String type;
+    private String section;
+    private String time;
+    private String days;
+    private String location;
+    private int creditHours;
+    private int crn;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public int getCrn() {
+        return crn;
+    }
+
+    public CourseInfo(String name, String type, String section, String time, String days, String location, int creditHours, int crn) {
+        this.name = name;
+        this.type = type;
+        this.section = section;
+        this.time = time;
+        this.days = days;
+        this.location = location;
+        this.creditHours = creditHours;
+        this.crn = crn;
+    }
 }
